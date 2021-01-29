0d166b6a18b7d5120db483a82c818998b09d6c03
mrmayurgithub <mr.mayurrrr@gmail.com>
2021-01-30 01:29:00 +0530

    minor changes

--- lib/ui/screens/home_page/home_page.dart
+++ lib/ui/screens/home_page/home_page.dart
@@ -7,44 +7,57 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Container(
-        width: screenWidth,
-        height: screenHeight,
-        child: Stack(
-          children: <Widget>[
-            ClipPath(
-              child: Container(
-                color: Colors.purple[500],
-                height: 140,
-                width: screenWidth,
-              ),
-              clipper: WaveClipperOne(),
-            ),
-            ClipPath(
-              child: Container(
-                color: Colors.purple[700],
-                height: 120,
-                width: screenWidth,
-              ),
-              clipper: WaveClipperTwo(),
-            ),
-            Positioned(
-              left: 60,
-              top: 20,
-              child: Text(
-                'OpinionX',
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                  fontSize: 30,
-                  fontFamily: 'Open Sans',
-                  letterSpacing: 0.2,
-                ),
-              ),
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        appBar: AppBar(
+          elevation: 0,
+          title: Text(
+            'Opinionx',
+            style: TextStyle(fontFamily: 'Open Sans', fontWeight: FontWeight.w700),
+          ),
+          //9,220,129
+          // backgroundColor: Color.fromARGB(255, 9, 220, 129),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Container(
+          width: screenWidth,
+          height: screenHeight,
+          child: Column(),
+          // child: Stack(
+          //   children: <Widget>[
+          //     ClipPath(
+          //       child: Container(
+          //         color: Colors.lightGreen[300],
+          //         height: 140,
+          //         width: screenWidth,
+          //       ),
+          //       clipper: WaveClipperOne(),
+          //     ),
+          //     ClipPath(
+          //       child: Container(
+          //         color: Colors.lightGreen[400],
+          //         height: 120,
+          //         width: screenWidth,
+          //       ),
+          //       clipper: WaveClipperTwo(),
+          //     ),
+          //     Positioned(
+          //       left: 60,
+          //       top: 20,
+          //       child: Text(
+          //         'OpinionX',
+          //         style: TextStyle(
+          //           fontWeight: FontWeight.w600,
+          //           color: Colors.black,
+          //           fontSize: 30,
+          //           fontFamily: 'Open Sans',
+          //           letterSpacing: 0.2,
+          //         ),
+          //       ),
+          //     ),
+          //   ],
+          // ),
         ),
       ),
     );
